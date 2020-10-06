5a05ebdccd9ff1b28f7fcdf5a3448fa7498a8612
Victor Norman <vtn2@calvin.edu>
2020-10-06 13:32:47 -0400

    create LogEntry type and make some dummy data

--- src/app/services/log-data.service.ts
+++ src/app/services/log-data.service.ts
@@ -1,36 +1,53 @@
 import { Injectable } from '@angular/core';
-import { IonDatetime } from '@ionic/angular';
+
+export interface LogEntry {
+  datetime: Date;
+  body_part: string;
+  intensity: number;
+  type: string;          // enumeration?
+  duration: number;      // in minutes
+  cause: string;
+  mobility: string;      // Moving, Resting, or Moving and Resting.  Use enumeration?
+  is_constant: boolean;  // assumes pain is either constant or intermittent
+  redflag_symptoms: any;  // TODO: figure out what type this is. Some kind of list probably. Whatever it is from ion-select with multiple option.
+  comment: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogDataService {
-  // TODO: extract to LogObject and LogDataService?
 
-  public loaded: boolean = false;
-  // datetime-page
-  private datetime: IonDatetime;  // TODO: initialize
-  // TODO: list of possible body parts? possible paint types? As class variables
+  public loaded = false;
 
-  // bodymap-page
-  private pain_body_part: String;
-
-  // symptomps-page
-  private pain_intensity: number;
-  private pain_type: String;  // ion-select
-  private pain_duration: number;  // minutes
-  private pain_cause: String;
-  private pain_mobility: String;  // Moving, Resting, or Moving and Resting. Use ion-select (possibly with multiple option?)
-  private pain_is_constant: boolean;  // assumes pain is either constant or intermittent
-
-  // redflags-page
-  private redflag_symptoms;  // TODO: figure out what type this is. Some kind of list probably. Whatever it is from ion-select with multiple option.
-
-  // comments-page
-  private comment: String;
+  private logEntries: LogEntry[] = [
+    {
+      datetime: new Date(),
+      body_part: "back",
+      intensity: 7,
+      type: "shooting pain",    // not what this field was intended for, i think.
+      duration: 120,
+      cause: "lifting",
+      mobility: "Moving",
+      is_constant: true,
+      redflag_symptoms: [],
+      comment: "we might need to store where on the body the pain is... like an x/y position?",
+    },
+    {
+      datetime: new Date(),
+      body_part: "left shoulder",
+      intensity: 7,
+      type: "dull ache",    // not what this field was intended for, i think.
+      duration: 120,
+      cause: "lifting",
+      mobility: "Moving",
+      is_constant: true,
+      redflag_symptoms: [],
+      comment: "we might need to store where on the body the pain is... like an x/y position?",
+    },
+  ];
 
   constructor() {
-
   }
 
   load(): Promise<boolean> {
