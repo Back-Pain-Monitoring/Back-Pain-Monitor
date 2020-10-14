60b6caa38ef3f989f1c8ff0f706de7575ba2c8ec
Leo-SIK <05rt06@gmail.com>
2020-10-14 14:08:36 -0400

    Created alert on submit button

--- src/app/comments-page/comments-page.page.ts
+++ src/app/comments-page/comments-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-comments-page',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsPagePage implements OnInit {
 
-  constructor() { }
+  constructor( private alertCtrl: AlertController) {
+
+   }
 
   ngOnInit() {
   }
+  // private toastCtrl: ToastController
+  // presentToast() {
+  //   let toast = this.toastCtrl.create({
+  //     message: 'User was added successfully',
+  //     duration: 3000,
+  //     position: 'top'
+  //   });
+  
+  //   toast.onDidDismiss(() => {
+  //     console.log('Dismissed toast');
+  //   });
+  
+  //   toast.present();
+  // }
 
+  makeAlert(): void {
+    this.alertCtrl.create({
+      header: 'Alert',
+      message: 'Your log is submitted',
+      inputs: [
+        {
+          type: 'text',
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+        }
+      ]
+    }).then((prompt) => {
+      prompt.present();
+    });
+  }
 }
