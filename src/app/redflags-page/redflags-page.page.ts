da26c43c8f48e71a84832c22587b332b52273a06
Leo-SIK <05rt06@gmail.com>
2020-10-17 15:07:59 -0400

    Added bit of work on dataservice

--- src/app/redflags-page/redflags-page.page.ts
+++ src/app/redflags-page/redflags-page.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LogDataService } from '../services/log-data.service';
+
 @Component({
   selector: 'app-redflags-page',
   templateUrl: './redflags-page.page.html',
   styleUrls: ['./redflags-page.page.scss'],
 })
+
 export class RedflagsPagePage implements OnInit {
 
-  constructor() { }
+  redflags_symptoms: string[];
+
+  constructor(public dataService: LogDataService) { 
+      this.dataService.logSubject.subscribe(data => { // TODO: add type
+      console.log("symptoms recieved updated log: " + data);
+    });
+  }
 
   ngOnInit() {
   }
+  
+  updateLog() {
+    this.dataService.currentLogRedflag_symptoms = this.redflags_symptoms;
+  }
 
+  updateUIFromLog() {
+    
+  }
 }
