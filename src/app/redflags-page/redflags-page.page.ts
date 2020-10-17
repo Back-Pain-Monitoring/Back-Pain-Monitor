f8d7710e62e01fa587aece2e3ae9764fdf54228d
Leo-SIK <05rt06@gmail.com>
2020-10-17 16:05:23 -0400

    Working on redflags data type conversion

--- src/app/redflags-page/redflags-page.page.ts
+++ src/app/redflags-page/redflags-page.page.ts
@@ -10,22 +10,55 @@ import { LogDataService } from '../services/log-data.service';
 
 export class RedflagsPagePage implements OnInit {
 
+  numbness: boolean;
+
+  public form = [
+    { val: 'numbness' , isChecked: false },
+    { val: 'inability', isChecked: false },
+    { val: 'losWeight' , isChecked: false },
+    { val: 'losBladder', isChecked: false }
+  ]
+
   redflags_symptoms: string[];
 
-  constructor(public dataService: LogDataService) { 
-      this.dataService.logSubject.subscribe(data => { // TODO: add type
-      console.log("symptoms recieved updated log: " + data);
-    });
+  constructor(public dataService: LogDataService) {
   }
 
   ngOnInit() {
   }
   
+  convertBoolToString() {
+    if ( this.form[0].isChecked == true ) {
+      this.redflags_symptoms.push("NumbnessTrue");
+    } else {
+      this.redflags_symptoms.push("NumbnessFalse");
+    }
+
+    if ( this.form[1].isChecked == true ) {
+      this.redflags_symptoms.push("InabilityTrue");
+    } else {
+      this.redflags_symptoms.push("InabilityFalse");
+    }
+    
+    if ( this.form[2].isChecked == true ) {
+      this.redflags_symptoms.push("LosingWeightTrue");
+    } else {
+      this.redflags_symptoms.push("LosingWeightFalse");
+    }
+    
+    if ( this.form[3].isChecked == true ) {
+      this.redflags_symptoms.push("LosingBladderTrue");
+    } else {
+      this.redflags_symptoms.push("LosingBladderFalse");
+    }
+  }
+
   updateLog() {
     this.dataService.currentLogRedflag_symptoms = this.redflags_symptoms;
+    this.dataService.printLogEntry();
   }
 
   updateUIFromLog() {
-    
+    this.redflags_symptoms = this.dataService.currentLogRedflag_symptoms;
   }
 }
