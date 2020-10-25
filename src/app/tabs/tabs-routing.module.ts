605cb815f4d35967b06eace3580477d4b88397de
CanadianMitch16 <mitchellniesar@gmail.com>
2020-10-25 19:28:12 -0400

    tabsFixes

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'viewlogs',
+        loadChildren: () => import('../viewlogs-page/viewlogs-page.module').then( m => m.ViewlogsPagePageModule)
+      },
+      {
+        path: 'sendlogs',
+        loadChildren: () => import('../send-logs-page/send-logs-page.module').then( m => m.SendLogsPagePageModule)
+      },
+      {
+        path: 'help',
+        loadChildren: () => import('../help-page/help-page.module').then( m => m.HelpPagePageModule)
+      },
+      {
+        path: 'insights',
+        loadChildren: () => import('../insights-page/insights-page.module').then( m => m.InsightsPagePageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
