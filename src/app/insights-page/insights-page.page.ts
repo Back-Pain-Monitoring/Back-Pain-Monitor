3ed04fc1923265def0fe8893886816e05909a423
CanadianMitch16 <mitchellniesar@gmail.com>
2020-11-14 12:10:14 -0500

    Medication service updates entries on submit

--- src/app/insights-page/insights-page.page.ts
+++ src/app/insights-page/insights-page.page.ts
@@ -19,6 +19,8 @@ export class InsightsPagePage implements OnInit {
   @ViewChild("constantPieCanvas") constantPieCanvas: ElementRef;
   @ViewChild("redflagsFreqCanvas") redflagsFreqCanvas: ElementRef;
   @ViewChild("medicationUseCanvas") medicationUseCanvas: ElementRef;
+  @ViewChild('nightPainPieCanvas') nightPainPieCanvas: ElementRef;
+  @ViewChild('worseBetterCanvas') worseBetterCanvas: ElementRef;
 
   // Creating the Chart objects
   private intensityTimeChart: Chart;
@@ -27,7 +29,9 @@ export class InsightsPagePage implements OnInit {
   private mobilityPieChart: Chart;
   private constantPieChart: Chart;
   private redflagsFreqChart: Chart;
-  private medicationUseChart: CharacterData;
+  private medicationUseChart: Chart;
+  private nightPainPieChart: CharacterData;
+  private worseBetterChart: CharacterData;
 
   private logsToDisplay = [];
   private medsToDisplay = [];
@@ -41,7 +45,6 @@ export class InsightsPagePage implements OnInit {
   }
 
   ngAfterViewInit() {
-
     // if this.logsToDisplay is not sorted by datetime, we need to do .sort((a, b) => a.datetime - b.datetime)
     const intensity_time_data = this.logsToDisplay.map(log => {
       return {
@@ -240,74 +243,74 @@ export class InsightsPagePage implements OnInit {
       }
     });
 
-    const nightData = [0, 0];
-    this.logsToDisplay.forEach(element => {
-      if (element.nightPain) {
-        nightData[0] += 1;
-      } else {
-        nightData[1] += 1;
-      }
-    });
+    // const nightData = [0, 0];
+    // this.logsToDisplay.forEach(element => {
+    //   if (element.nightPain) {
+    //     nightData[0] += 1;
+    //   } else {
+    //     nightData[1] += 1;
+    //   }
+    // });
 
-    this.nightPainPieChart = new Chart(this.nightPainPieCanvas.nativeElement, {
-      type: "pie",
-      data: {
-        labels: ["No", "Yes"],
-        datasets: [
-          {
-            data: nightData,
-            backgroundColor: ['#003f5c', '#bc5090']
-          }
-        ]
-      }
-    });
+    // this.nightPainPieChart = new Chart(this.nightPainPieCanvas.nativeElement, {
+    //   type: "pie",
+    //   data: {
+    //     labels: ["No", "Yes"],
+    //     datasets: [
+    //       {
+    //         data: nightData,
+    //         backgroundColor: ['#003f5c', '#bc5090']
+    //       }
+    //     ]
+    //   }
+    // });
 
-    const worse_fd = this.createFreqDist(this.logsToDisplay, "worse", true);
-    const better_fd = this.createFreqDist(this.logsToDisplay, "better", true);
-    const worse_better_labels = this.dataService.activities;
+    // const worse_fd = this.createFreqDist(this.logsToDisplay, "worse", true);
+    // const better_fd = this.createFreqDist(this.logsToDisplay, "better", true);
+    // const worse_better_labels = this.dataService.activities;
 
-    console.log(worse_better_labels.map(element => {
-      return worse_fd[element] || 0;
-    }))
+    // console.log(worse_better_labels.map(element => {
+    //   return worse_fd[element] || 0;
+    // }))
 
-    this.worseBetterChart = new Chart(this.worseBetterCanvas.nativeElement, {
-      type: "bar",
-      data: {
-        labels: worse_better_labels,
-        datasets: [
-          {
-            label: "worse",
-            data: worse_better_labels.map(element => {
-              return worse_fd[element] || 0;
-            }),
-            // fillColor: "blue",
-            backgroundColor: '#003f5c', // array should have same number of elements as number of dataset
-            // borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-            // borderWidth: 1
-          },
-          {
-            label: "better",
-            data: worse_better_labels.map(element => {
-              return better_fd[element] || 0;
-            }),
-            // fillColor: "red",
-            backgroundColor: '#bc5090', // array should have same number of elements as number of dataset
-            // borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-            // borderWidth: 1
-          },
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              precision: 0,
-            }
-          }]
-        }
-      }
-    });
+    // this.worseBetterChart = new Chart(this.worseBetterCanvas.nativeElement, {
+    //   type: "bar",
+    //   data: {
+    //     labels: worse_better_labels,
+    //     datasets: [
+    //       {
+    //         label: "worse",
+    //         data: worse_better_labels.map(element => {
+    //           return worse_fd[element] || 0;
+    //         }),
+    //         // fillColor: "blue",
+    //         backgroundColor: '#003f5c', // array should have same number of elements as number of dataset
+    //         // borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+    //         // borderWidth: 1
+    //       },
+    //       {
+    //         label: "better",
+    //         data: worse_better_labels.map(element => {
+    //           return better_fd[element] || 0;
+    //         }),
+    //         // fillColor: "red",
+    //         backgroundColor: '#bc5090', // array should have same number of elements as number of dataset
+    //         // borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+    //         // borderWidth: 1
+    //       },
+    //     ]
+    //   },
+    //   options: {
+    //     scales: {
+    //       yAxes: [{
+    //         ticks: {
+    //           beginAtZero: true,
+    //           precision: 0,
+    //         }
+    //       }]
+    //     }
+    //   }
+    // });
 
 
     const medication_fd = { "NSAID": 0, "Acetaminiophen": 0, "COX-2 Inhibitors": 0, "Antidepressants": 0, "Anti-Seizure": 0 };
