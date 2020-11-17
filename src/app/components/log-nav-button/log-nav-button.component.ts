a59cb899f46d0f102bd8ce4e1ac64884d450d83f
cmd16 <catherine.dejager10@gmail.com>
2020-11-17 17:14:24 -0500

    fix nav button component so alert only shows up once

--- src/app/components/log-nav-button/log-nav-button.component.ts
+++ src/app/components/log-nav-button/log-nav-button.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { LogDataService } from 'src/app/services/log-data.service';
 
 @Component({
@@ -12,38 +12,37 @@ export class LogNavButtonComponent implements OnInit {
 
   @Input() direction: "forward" | "backward";
   @Input() link: string;
-  private subscription: Subscription;
+
+  private navRequested: boolean = false;
 
   constructor(private navCtrl: NavController, private dataService: LogDataService) { }
 
   ngOnInit() {
+    this.dataService.isEnteredSubj.subscribe((data) => {
+      if (data === true) {
+        this.navigate();
+      }
+    });
+  }
 
+  requestNav() {
+    this.navRequested = true;
+    console.log(`requesting nav for ${this.direction}`);
   }
 
   navigate() {
-
-    console.log('navigating');
-
-    // if (this.subscription) {
-    //   console.log('unsubscribing');
-    //   this.subscription.unsubscribe();
-    //   this.subscription = null;
-    // }
-
-    this.subscription = this.dataService.isEnteredSubj.subscribe((is_entered) => {
-      if (is_entered) {
-        console.log('activating navCtrl');
-        if (this.direction === "forward") {
-          this.navCtrl.navigateForward(this.link);
-        } else {
-          this.navCtrl.navigateBack(this.link);
-        }
-        if (this.subscription) {
-          this.subscription.unsubscribe();
-        }
-      }
-    })
-
+    if (!this.navRequested) {
+      console.log(`nav not requested for ${this.direction}`);
+      return;
+    }
+    console.log('activating navCtrl');
+    if (this.direction === "forward") {
+      this.navCtrl.navigateForward(this.link);
+    } else {
+      this.navCtrl.navigateBack(this.link);
+    }
+    this.navRequested = false;
   }
+
 
 }
