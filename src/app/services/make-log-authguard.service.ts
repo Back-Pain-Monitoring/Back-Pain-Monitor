f40048146135d2ca24465cd132e7ada5c245310e
cmd16 <catherine.dejager10@gmail.com>
2021-02-21 14:59:26 -0500

    display current user, prevent log creation when not signed in.

---
+++ src/app/services/make-log-authguard.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MakeLogAuthguardService implements CanActivate {
+
+  constructor(private router: Router, private authSvc: AuthService, private alertCtrl: AlertController) { }
+
+  canActivate(): boolean {
+    if (!this.authSvc.uidSubj.value) {
+      this.alertCtrl.create({
+        message: 'You need to sign in before you can create a log.',
+        buttons: ['OK']
+      }).then((prompt) => {
+        prompt.present();
+      });
+      return false;
+    }
+    return true;
+  }
+}
