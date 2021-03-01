c244fda6699be84d9276754a651e93889d65906b
Leo-SIK <05rt06@gmail.com>
2021-02-28 20:08:51 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +27,7 @@ import { firebaseConfig } from './credentials';
   providers: [
     StatusBar,
     SplashScreen,
+    FileOpener,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/insights-page/insights-page.page.ts
+++ src/app/insights-page/insights-page.page.ts
@@ -3,9 +3,14 @@ import { Component, ComponentFactoryResolver, ElementRef, OnInit, ViewChild } fr
 import { Chart } from 'chart.js';
 import { LogDataService, LogFilter } from '../services/log-data.service';
 import { FilterModalPageComponent } from '../filter-modal/filter-modal.component';
-import { ModalController } from '@ionic/angular';
+import { ModalController, LoadingController } from '@ionic/angular';
 import { element } from 'protractor';
 import { MedicationDataService } from '../services/medication-data.service';
+
+import * as jsPDF from 'jspdf';
+import domtoimage from 'dom-to-image';
+import { File, IWriteOptions } from '@ionic-native/file/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
 
 @Component({
   selector: 'app-insights-page',
@@ -42,7 +47,78 @@ export class InsightsPagePage {
   private logsToDisplay = [];
   private medsToDisplay = [];
 
-  constructor(private dataService: LogDataService, public modalCtrl: ModalController, private MedService: MedicationDataService) {
+  loading: any;
+
+  constructor(private dataService: LogDataService, public modalCtrl: ModalController, private MedService: MedicationDataService, public loadingCtrl: LoadingController, private fileOpener: FileOpener ) {
+  }
+
+  async exportButton(msg) {
+    const presentLoading = await this.loadingCtrl.create({
+      message: msg
+    })
+    return await loading.present();
+  }
+
+  exportPdf() {
+    this.presentLoading('Creating PDF file...');
+    const div = document.getElementById("printable-area");
+    const options = { background: "white", height: div.clientWidth, width: div.clientHeight };
+    domtoimage.toPng(div, options).then((dataUrl)=> {
+      //Initialize JSPDF
+      var doc = new jsPDF("p","mm","a4");
+      //Add image Url to PDF
+      doc.addImage(dataUrl, 'PNG', 20, 20, 240, 180);
+  
+      let pdfOutput = doc.output();
+      // using ArrayBuffer will allow you to put image inside PDF
+      let buffer = new ArrayBuffer(pdfOutput.length);
+      let array = new Uint8Array(buffer);
+      for (var i = 0; i < pdfOutput.length; i++) {
+          array[i] = pdfOutput.charCodeAt(i);
+      }
+  
+  
+      //This is where the PDF file will stored , you can change it as you like
+      // for more information please visit https://ionicframework.com/docs/native/file/
+      const directory = this.file.dataDirectory ;
+      const fileName = "invoice.pdf";
+      let options: IWriteOptions = { replace: true };
+  
+      this.file.checkFile(directory, fileName).then((success)=> {
+        //Writing File to Device
+        this.file.writeFile(directory,fileName,buffer, options)
+        .then((success)=> {
+          this.loading.dismiss();
+          console.log("File created Succesfully" + JSON.stringify(success));
+          this.fileOpener.open(this.file.dataDirectory + fileName, 'application/pdf')
+            .then(() => console.log('File is opened'))
+            .catch(e => console.log('Error opening file', e));
+        })
+        .catch((error)=> {
+          this.loading.dismiss();
+          console.log("Cannot Create File " +JSON.stringify(error));
+        });
+      })
+      .catch((error)=> {
+        //Writing File to Device
+        this.file.writeFile(directory,fileName,buffer)
+        .then((success)=> {
+          this.loading.dismiss();
+          console.log("File created Succesfully" + JSON.stringify(success));
+          this.fileOpener.open(this.file.dataDirectory + fileName, 'application/pdf')
+            .then(() => console.log('File is opened'))
+            .catch(e => console.log('Error opening file', e));
+        })
+        .catch((error)=> {
+          this.loading.dismiss();
+          console.log("Cannot Create File " +JSON.stringify(error));
+        });
+      });
+    })
+    .catch(function (error) {
+      this.loading.dismiss();
+      console.error('oops, something went wrong!', error);
+    });
   }
 
   ionViewWillEnter() {
